b1110b906c3b5a3a317ebbd26b2233e9f7e2c605
“ourcwj” <1961894636@qq.com>
2022-03-30 01:14:37 +0800

    updata some cold

--- ver/MoyuSystem/MS-BIOS/__init__.py
+++ ver/MoyuSystem/MS-BIOS/__init__.py
@@ -1,2 +1,11 @@
 import os
 import time
+
+class BIOS():
+    def __init__(self):
+        self.test = "test"
+        print("BIOS打印了一个", self.test)
+        self.test
+
+    def test(self):
+        print("调用函数test，打印了", self.test)
--- ver/MoyuSystem/start.py
+++ ver/MoyuSystem/start.py
@@ -1,3 +1,12 @@
 import os
 import time
+import MS-BIOS
+
+def main():
+    bios = MS-BIOS.BIOS()
+
+if __name__ == "__main__":
+    main()
+
+
 
