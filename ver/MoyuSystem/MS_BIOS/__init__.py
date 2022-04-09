bf61cc10c7760023acd8fbb32ffeeabdeb990f7f
ourcwj <1961894636@qq.com>
2022-04-09 12:24:23 +0800

    优化了初始化函数，使添加配置文件更加便捷

--- ver/MoyuSystem/MS_BIOS/__init__.py
+++ ver/MoyuSystem/MS_BIOS/__init__.py
@@ -1,3 +1,9 @@
+# -- UTF-8 --
+# date: 2022年4月9日
+# made by: ourcwj
+
+# 注意这里所有的 print 函数均是为了开发调试需要，可以删除（特殊备注除外）
+
 import configparser
 import os
 import time
@@ -6,7 +12,12 @@ import time
 class BIOS():
     def __init__(self):
         #初始化ms_bios的数据
-        self.tmp_fils = ['readme.txt']
+
+        # 如果要加入配置文件则在此列表中添加文件（有文件后缀）
+        self.wirte_fils = ['readme.txt', 'test.txt']
+        # 然后在这个字典内添加文件名（没有后缀）和文件内容
+        self.content_fils = {'readme' : '写入测试', 
+                             'test' : 'test\nthe another test'}
         self.appdataRoute = os.getenv('appdata')     # 获得appdata绝对的路径
         file = self.appdataRoute + '\\ms'  # 设置ms的路径
         if not os.path.isdir(file):   # 判断路径是否存在
@@ -23,24 +34,32 @@ class BIOS():
 
     def init(self):
         # MS初始化函数
+        fileName_key = []
         fils = {}
 
         def addFils():
-            for i in self.tmp_fils:
-                # 分离路径，文件名称及后缀。并把绝对路径添加至fils字典
+            for i in self.wirte_fils:
+                # 分离路径，文件名称及后缀。并把绝对路径添加至fils字典, 将文件名（key）加入fileName_key列表
                 tmp = self.appdataRoute+i
                 print('tmp:', tmp)
                 (route, fileName) = os.path.split(tmp)
                 print('file name:', fileName)
                 (name, suffix) = os.path.splitext(fileName)
                 print('name:', name)
-
+                fileName_key.append(name)
                 fils['%s' % (name)] = tmp
             print(fils)
 
         def write():
-            with open(fils['readme'], 'w') as tmp:
-                tmp.write('写入测试')
+            # 数据写入函数
+            for i in fileName_key:
+                # 利用递归fileName_key顺次读取fils的数据用于创建文件，并使用fileName_key的数据读取content_fils字典的内容用于写入
+                with open(fils[i], 'w') as tmp:
+                    tmp.write(self.content_fils[i])
+
+
+            # with open(fils['readme'], 'w') as tmp:
+            #     tmp.write('写入测试')
 
 
         addFils()
@@ -56,7 +75,7 @@ class BIOS():
 
         fils = []
 
-        for i in self.tmp_fils:
+        for i in self.wirte_fils:
             # 建立将要检擦的文件的绝对路径
             fils.append(self.appdataRoute+ "%s" % (i))
         # print(fils)
