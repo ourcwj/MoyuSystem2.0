c84830decca8b259bcb01b4add6ce803549fbcf6
ourcwj <1961894636@qq.com>
2022-04-25 23:06:26 +0800

    生气

--- ver/MoyuSystem/MS_BIOS/execute.py
+++ ver/MoyuSystem/MS_BIOS/execute.py
@@ -12,12 +12,25 @@ def add():
     pass
 
 def readFile(filename):
+    rou = os.getenv('temp') + '\\ms\\'
+    if not os.path.isdir(rou):
+        os.makedirs(rou)
     tmp = {}
-    test = configobj.ConfigObj(filename, encoding='UTF8')
+    test = configobj.ConfigObj(filename, encoding='UTF-8')
     tmp['name'] = test['name']['name']
+    tmp_rou = rou + test['data']['photoname']
+    if os.path.isfile(tmp_rou):
+        os.remove(tmp_rou)
+    with open(tmp_rou, mode='wb') as t:
+        t.write(str.encode(test['data']['dataP']))
+    # tmp['photo'] = test['data']['dataP']
+    tmp['photo'] = tmp_rou
+    tmp['answer'] = test['data']['dataA']
+    return tmp
     
 def weitefile(route, name, dataP, dataA):
     datapdata = ''
+    datapdata = bytes(datapdata, encoding='UTF-8')
     for line in open(dataP, mode='rb'):
         datapdata = datapdata + line
     tmp = {
@@ -26,16 +39,18 @@ def weitefile(route, name, dataP, dataA):
         'dataAn' : dataA
     }
 
-    file = route + name + '.msdata'
+    file = route + '\\' + name + '.msdata'
     if not os.path.isfile(file):
         with open(file, mode='w', encoding='UTF-8') as t:
             t.close()
     else:
         return False
     test = configobj.ConfigObj(file, encoding='UTF-8')
+    (tmp_123, fileName) = os.path.split(dataP)
     test['name'] = {}
     test['data'] = {}
     test['name']['name'] = tmp['name']
     test['data']['dataP'] = tmp['dataphoto']
     test['data']['dataA'] = tmp['dataAn']
+    test['data']['photoname'] = fileName
     test.write()
